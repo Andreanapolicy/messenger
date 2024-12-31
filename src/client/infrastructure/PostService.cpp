aa0d20d279b9a83b066b0f087362cc7342250023
Andreanapolicy <daa.and@mail.ru>
2024-12-31 12:22:21 +0300

    Send to the server message, very dump realization

--- src/client/infrastructure/PostService.cpp
+++ src/client/infrastructure/PostService.cpp
@@ -4,6 +4,11 @@
 #include "PostService.h"
 #include <iostream> // todo: delete
 
+#include <cstring>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 namespace client::infrastructure
 {
 PostService::PostService()
@@ -12,6 +17,23 @@ PostService::PostService()
 
 void PostService::SendMesssage(app::MessageData data)
 {
-	std::cout << "message " << data.data << " was sent" << std::endl;
+	// creating socket
+	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// specifying address
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+	// sending connection request
+	connect(clientSocket, (struct sockaddr*)&serverAddress,
+		sizeof(serverAddress));
+
+	// sending data
+	send(clientSocket, data.data.c_str(), data.data.size(), 0);
+
+	// closing socket
+	close(clientSocket);
 }
 } // namespace client::infrastructure
